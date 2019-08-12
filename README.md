cc0c0ffa4e74ec2cddf768f9cf927bf3383a90c5
Shrirang Kadale <skadale@altimetrik.com>
2019-08-12 16:23:23 +0530

    Committed the code

--- README.md
+++ README.md
@@ -33,3 +33,4 @@
 -One service class which will invoke the remote API and this will send exception in case of failure.
 -We will design the retry based on this custom exception, like once we receive this exception, we will retry for 3 times and finally return to the client.
 -In those 3 attempts, if we get success response from backend service then that success response will be returned else the standard fallback method will be called.
+# spring-retry
---
+++ src/main/java/com/example/springretry/BackendAdapterImpl.java
@@ -0,0 +1,45 @@
+package com.example.springretry;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BackendAdapterImpl implements BackendAdapter {
+
+	@Override
+	public String getBackendResponse(boolean simulateretry, boolean simulateretryfallback) {
+
+		if (simulateretry) {
+			System.out.println("Simulateretry is true, so try to simulate exception scenerio.");
+
+			// This block will execute until max attempts of spring retry and afterwards fallback method gets called
+			if (simulateretryfallback) {
+				throw new RemoteServiceNotAvailableException(
+						"Don't worry!! Just Simulated for Spring-retry..Must fallback as all retry will get exception!!!");
+			}
+			
+			// Here I can not call the remote service so I am creating the random number between (0, 1, 2, 3)
+			// If 1 or 3 gets generated then pretending that the remote method gets called with successful response
+			// Logic to generate the random number from 0 (inclusive) to 3
+			int random = new Random().nextInt(4);
+
+			System.out.println("Random Number : " + random);
+			
+			// If 0 or 2 gets generated then pretending that remote method called with unsuccessful response that is exception
+			if (random % 2 == 0) {
+				throw new RemoteServiceNotAvailableException("Don't worry!! Just Simulated for Spring-retry..");
+			}
+		}
+
+		return "Hello from Remote Backend!!!";
+	}
+
+	// Fallback method
+	// This method gets called when all retry operations failed/remote service is down
+	@Override
+	public String getBackendResponseFallback(RuntimeException e) {
+		System.out.println("All retries completed, so Fallback method called!!!");
+		return "All retries completed, so Fallback method called!!!";
+	}
+}
